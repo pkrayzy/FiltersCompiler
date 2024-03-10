a46a143e30d85641dc655151ddb7f33feb602d9b
Filters Builder <filters@adguard.com>
2024-03-10 01:37:17 +0000

    skip ci. build from Sun Mar 10 01:37:17 UTC 2024

--- platforms/extension/android-content-blocker/filters.js
+++ platforms/extension/android-content-blocker/filters.js
@@ -357,8 +357,8 @@
 			"groupId": 1,
 			"subscriptionUrl": "https://filters.adtidy.org/extension/android-content-blocker/filters/2.txt",
 			"trustLevel": "full",
-			"version": "2.3.26.0",
-			"timeUpdated": "2024-03-09T23:33:06+0000",
+			"version": "2.3.26.1",
+			"timeUpdated": "2024-03-10T01:32:54+0000",
 			"languages": [],
 			"tags": [
 				10,
