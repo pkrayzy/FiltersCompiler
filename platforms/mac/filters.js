d997b1a4f301c827bfef8b98cc80b3d6da636aac
Filters Builder <filters@adguard.com>
2024-03-18 19:38:37 +0000

    skip ci. build from Mon Mar 18 19:38:37 UTC 2024

--- platforms/mac/filters.js
+++ platforms/mac/filters.js
@@ -61,8 +61,8 @@
 			"displayNumber": 1,
 			"groupId": 1,
 			"subscriptionUrl": "https://filters.adtidy.org/mac/filters/2.txt",
-			"version": "2.3.27.24",
-			"timeUpdated": "2024-03-18T16:34:21+0000",
+			"version": "2.3.27.25",
+			"timeUpdated": "2024-03-18T19:33:13+0000",
 			"languages": []
 		},
 		{
@@ -74,8 +74,8 @@
 			"displayNumber": 1,
 			"groupId": 2,
 			"subscriptionUrl": "https://filters.adtidy.org/mac/filters/3.txt",
-			"version": "2.0.67.84",
-			"timeUpdated": "2024-03-18T17:33:55+0000",
+			"version": "2.0.67.85",
+			"timeUpdated": "2024-03-18T19:33:22+0000",
 			"languages": []
 		},
 		{
@@ -228,8 +228,8 @@
 			"displayNumber": 1,
 			"groupId": 4,
 			"subscriptionUrl": "https://filters.adtidy.org/mac/filters/14.txt",
-			"version": "2.1.90.93",
-			"timeUpdated": "2024-03-18T17:32:31+0000",
+			"version": "2.1.90.94",
+			"timeUpdated": "2024-03-18T19:32:10+0000",
 			"languages": []
 		},
 		{
@@ -287,8 +287,8 @@
 			"displayNumber": 2,
 			"groupId": 4,
 			"subscriptionUrl": "https://filters.adtidy.org/mac/filters/18.txt",
-			"version": "2.0.35.93",
-			"timeUpdated": "2024-03-18T16:33:23+0000",
+			"version": "2.0.35.94",
+			"timeUpdated": "2024-03-18T19:32:21+0000",
 			"languages": []
 		},
 		{
@@ -300,8 +300,8 @@
 			"displayNumber": 2,
 			"groupId": 4,
 			"subscriptionUrl": "https://filters.adtidy.org/mac/filters/19.txt",
-			"version": "2.0.33.52",
-			"timeUpdated": "2024-03-18T16:33:27+0000",
+			"version": "2.0.33.53",
+			"timeUpdated": "2024-03-18T19:32:25+0000",
 			"languages": []
 		},
 		{
