8540af9a228bbbc65d3c80609a076edd68f02a78
Filters Builder <filters@adguard.com>
2024-03-18 20:37:44 +0000

    skip ci. build from Mon Mar 18 20:37:44 UTC 2024

--- platforms/extension/ublock/filters.js
+++ platforms/extension/ublock/filters.js
@@ -357,8 +357,8 @@
 			"groupId": 1,
 			"subscriptionUrl": "https://filters.adtidy.org/extension/ublock/filters/2.txt",
 			"trustLevel": "full",
-			"version": "2.3.27.25",
-			"timeUpdated": "2024-03-18T19:33:13+0000",
+			"version": "2.3.27.26",
+			"timeUpdated": "2024-03-18T20:33:06+0000",
 			"languages": [],
 			"tags": [
 				10,
--- platforms/mac/filters.js
+++ platforms/mac/filters.js
@@ -61,8 +61,8 @@
 			"displayNumber": 1,
 			"groupId": 1,
 			"subscriptionUrl": "https://filters.adtidy.org/mac/filters/2.txt",
-			"version": "2.3.27.25",
-			"timeUpdated": "2024-03-18T19:33:13+0000",
+			"version": "2.3.27.26",
+			"timeUpdated": "2024-03-18T20:33:06+0000",
 			"languages": []
 		},
 		{
