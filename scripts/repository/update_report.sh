76942102b60b0aa8428a8f9c69ddb1023e2cc08e
Slava Leleka <v.leleka@adguard.com>
2024-04-04 15:58:31 +0300

    log Revert "reset filters and platforms directories before report.txt push"
    This reverts commit 1cfc6899efff32535bf82dc469bdf9ff4d36baa2.

--- scripts/repository/update_report.sh
+++ scripts/repository/update_report.sh
@@ -3,6 +3,5 @@ set -x -e
 # Push updated report.txt to the repo.
 git status
 git add report.txt
-git checkout filters/ platforms/
 git diff-index --quiet HEAD || git commit -m "skip ci. update report.txt from $(date)"
 git push origin master
