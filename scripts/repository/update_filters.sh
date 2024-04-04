447af24af2b735b4fe5e8fca40936ee895a962a5
Slava Leleka <v.leleka@adguard.com>
2024-04-04 15:45:10 +0300

    update report.txt during full filters build

--- scripts/repository/update_filters.sh
+++ scripts/repository/update_filters.sh
@@ -2,7 +2,6 @@ set -x -e
 
 # Push updated filter lists to the repo (without patches).
 git status
-git add report.txt
 git add filters
 git add platforms
 git diff-index --quiet HEAD || git commit -m "skip ci. build from $(date)"
---
+++ scripts/repository/update_report.sh
@@ -0,0 +1,7 @@
+set -x -e
+
+# Push updated report.txt to the repo.
+git status
+git add report.txt
+git diff-index --quiet HEAD || git commit -m "skip ci. update report.txt from $(date)"
+git push origin master
