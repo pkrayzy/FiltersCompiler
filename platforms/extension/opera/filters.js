0fb141dd1e911418eb645acdfc55883262b688f3
Filters Builder <filters@adguard.com>
2024-04-12 03:38:15 +0000

    skip ci. build from Fri Apr 12 03:38:15 UTC 2024

--- platforms/extension/opera/filters.js
+++ platforms/extension/opera/filters.js
@@ -357,8 +357,8 @@
 			"groupId": 1,
 			"subscriptionUrl": "https://filters.adtidy.org/extension/opera/filters/2.txt",
 			"trustLevel": "full",
-			"version": "2.3.30.76",
-			"timeUpdated": "2024-04-12T02:32:16+0000",
+			"version": "2.3.30.77",
+			"timeUpdated": "2024-04-12T03:32:17+0000",
 			"languages": [],
 			"tags": [
 				10,
--- platforms/mac/filters.json
+++ platforms/mac/filters.json
@@ -61,8 +61,8 @@
 			"displayNumber": 1,
 			"groupId": 1,
 			"subscriptionUrl": "https://filters.adtidy.org/mac/filters/2.txt",
-			"version": "2.3.30.76",
-			"timeUpdated": "2024-04-12T02:32:16+0000",
+			"version": "2.3.30.77",
+			"timeUpdated": "2024-04-12T03:32:17+0000",
 			"languages": []
 		},
 		{
