322a10b15fd39abc9d93167dcb61e151977cd627
Filters Builder <filters@adguard.com>
2024-04-28 05:34:31 +0000

    skip ci. build from Sun Apr 28 05:34:31 UTC 2024

--- platforms/android/filters.js
+++ platforms/android/filters.js
@@ -357,8 +357,8 @@
 			"groupId": 1,
 			"subscriptionUrl": "https://filters.adtidy.org/android/filters/2_optimized.txt",
 			"trustLevel": "full",
-			"version": "2.3.33.8",
-			"timeUpdated": "2024-04-27T23:31:11+0000",
+			"version": "2.3.33.9",
+			"timeUpdated": "2024-04-28T05:31:17+0000",
 			"languages": [],
 			"tags": [
 				10,
--- platforms/mac/filters.json
+++ platforms/mac/filters.json
@@ -61,8 +61,8 @@
 			"displayNumber": 1,
 			"groupId": 1,
 			"subscriptionUrl": "https://filters.adtidy.org/mac/filters/2.txt",
-			"version": "2.3.33.8",
-			"timeUpdated": "2024-04-27T23:31:11+0000",
+			"version": "2.3.33.9",
+			"timeUpdated": "2024-04-28T05:31:17+0000",
 			"languages": []
 		},
 		{
