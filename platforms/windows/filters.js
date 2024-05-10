542a701a057b5954e89a784407d2e275afc1f3f6
Filters Builder <filters@adguard.com>
2024-05-10 15:34:41 +0000

    skip ci. build from Fri May 10 15:34:41 UTC 2024

--- platforms/windows/filters.js
+++ platforms/windows/filters.js
@@ -357,8 +357,8 @@
 			"groupId": 1,
 			"subscriptionUrl": "https://filters.adtidy.org/windows/filters/2.txt",
 			"trustLevel": "full",
-			"version": "2.3.35.4",
-			"timeUpdated": "2024-05-10T14:31:17+0000",
+			"version": "2.3.35.5",
+			"timeUpdated": "2024-05-10T15:31:15+0000",
 			"languages": [],
 			"tags": [
 				10,
