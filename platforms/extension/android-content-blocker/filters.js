e34ca239a482655b23b23ce1771c1ba1994666cb
Filters Builder <filters@adguard.com>
2024-05-11 06:34:38 +0000

    skip ci. build from Sat May 11 06:34:38 UTC 2024

--- platforms/extension/android-content-blocker/filters.js
+++ platforms/extension/android-content-blocker/filters.js
@@ -607,8 +607,8 @@
 			"groupId": 6,
 			"subscriptionUrl": "https://filters.adtidy.org/extension/android-content-blocker/filters/15.txt",
 			"trustLevel": "full",
-			"version": "2.0.65.28",
-			"timeUpdated": "2024-05-11T00:31:56+0000",
+			"version": "2.0.65.29",
+			"timeUpdated": "2024-05-11T06:31:50+0000",
 			"languages": [],
 			"tags": [
 				45,
--- platforms/mac/filters.json
+++ platforms/mac/filters.json
@@ -241,8 +241,8 @@
 			"displayNumber": 3,
 			"groupId": 6,
 			"subscriptionUrl": "https://filters.adtidy.org/mac/filters/15.txt",
-			"version": "2.0.65.28",
-			"timeUpdated": "2024-05-11T00:31:56+0000",
+			"version": "2.0.65.29",
+			"timeUpdated": "2024-05-11T06:31:50+0000",
 			"languages": []
 		},
 		{
