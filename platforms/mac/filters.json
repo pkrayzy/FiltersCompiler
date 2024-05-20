ee6648736a7b5b83a0283955a4d2d53799d2dae2
Filters Builder <filters@adguard.com>
2024-05-20 04:34:09 +0000

    skip ci. build from Mon May 20 04:34:08 UTC 2024

--- platforms/mac/filters.json
+++ platforms/mac/filters.json
@@ -61,8 +61,8 @@
 			"displayNumber": 1,
 			"groupId": 1,
 			"subscriptionUrl": "https://filters.adtidy.org/mac/filters/2.txt",
-			"version": "2.3.36.32",
-			"timeUpdated": "2024-05-20T03:31:24+0000",
+			"version": "2.3.36.33",
+			"timeUpdated": "2024-05-20T04:31:08+0000",
 			"languages": []
 		},
 		{
--- platforms/mac_v2/filters.js
+++ platforms/mac_v2/filters.js
@@ -357,8 +357,8 @@
 			"groupId": 1,
 			"subscriptionUrl": "https://filters.adtidy.org/mac_v2/filters/2.txt",
 			"trustLevel": "full",
-			"version": "2.3.36.32",
-			"timeUpdated": "2024-05-20T03:31:24+0000",
+			"version": "2.3.36.33",
+			"timeUpdated": "2024-05-20T04:31:08+0000",
 			"languages": [],
 			"tags": [
 				10,
