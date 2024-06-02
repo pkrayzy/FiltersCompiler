c115a180e98c1350285a41c6b47bd58d6a1abb47
Filters Builder <filters@adguard.com>
2024-06-02 03:36:00 +0000

    skip ci. build from Sun Jun  2 03:36:00 UTC 2024

--- platforms/ios/filters.js
+++ platforms/ios/filters.js
@@ -357,8 +357,8 @@
 			"groupId": 1,
 			"subscriptionUrl": "https://filters.adtidy.org/ios/filters/2_optimized.txt",
 			"trustLevel": "full",
-			"version": "2.3.38.25",
-			"timeUpdated": "2024-06-02T01:31:36+0000",
+			"version": "2.3.38.26",
+			"timeUpdated": "2024-06-02T03:31:36+0000",
 			"languages": [],
 			"tags": [
 				10,
