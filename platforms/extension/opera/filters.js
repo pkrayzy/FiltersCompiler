dcc81751f45ee3fac2efe70a432ca4b32904ef2a
Filters Builder <filters@adguard.com>
2024-06-18 23:35:28 +0000

    skip ci. build from Tue Jun 18 23:35:28 UTC 2024

--- platforms/extension/opera/filters.js
+++ platforms/extension/opera/filters.js
@@ -1506,8 +1506,8 @@
 			"groupId": 7,
 			"subscriptionUrl": "https://filters.adtidy.org/extension/opera/filters/224.txt",
 			"trustLevel": "full",
-			"version": "2.1.17.58",
-			"timeUpdated": "2024-06-18T16:33:45+0000",
+			"version": "2.1.17.59",
+			"timeUpdated": "2024-06-18T23:33:18+0000",
 			"languages": [
 				"zh"
 			],
