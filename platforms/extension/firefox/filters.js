80509869a23a0e2eea52e9e3c6c07243d2016306
Filters Builder <filters@adguard.com>
2024-07-12 20:37:21 +0000

    skip ci. build from Fri Jul 12 20:37:21 UTC 2024

--- platforms/extension/firefox/filters.js
+++ platforms/extension/firefox/filters.js
@@ -358,8 +358,8 @@
 			"groupId": 1,
 			"subscriptionUrl": "https://filters.adtidy.org/extension/firefox/filters/2.txt",
 			"trustLevel": "full",
-			"version": "2.3.43.75",
-			"timeUpdated": "2024-07-12T17:31:18+0000",
+			"version": "2.3.43.76",
+			"timeUpdated": "2024-07-12T20:31:22+0000",
 			"languages": [],
 			"tags": [
 				10,
