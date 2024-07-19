cdcb9277cedcf5b183c46643df987037fc7e811d
Filters Builder <filters@adguard.com>
2024-07-19 17:37:18 +0000

    skip ci. build from Fri Jul 19 17:37:18 UTC 2024

--- platforms/windows/filters.js
+++ platforms/windows/filters.js
@@ -358,8 +358,8 @@
 			"groupId": 1,
 			"subscriptionUrl": "https://filters.adtidy.org/windows/filters/2.txt",
 			"trustLevel": "full",
-			"version": "2.3.44.68",
-			"timeUpdated": "2024-07-19T16:31:27+0000",
+			"version": "2.3.44.69",
+			"timeUpdated": "2024-07-19T17:31:24+0000",
 			"languages": [],
 			"tags": [
 				10,
