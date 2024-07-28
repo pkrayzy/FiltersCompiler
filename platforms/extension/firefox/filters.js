44f47c2ee512c14313b96308e26c075d5812bc99
Filters Builder <filters@adguard.com>
2024-07-28 22:37:06 +0000

    skip ci. build from Sun Jul 28 22:37:06 UTC 2024

--- platforms/extension/firefox/filters.js
+++ platforms/extension/firefox/filters.js
@@ -358,8 +358,8 @@
 			"groupId": 1,
 			"subscriptionUrl": "https://filters.adtidy.org/extension/firefox/filters/2.txt",
 			"trustLevel": "full",
-			"version": "2.3.45.82",
-			"timeUpdated": "2024-07-28T18:31:21+0000",
+			"version": "2.3.45.83",
+			"timeUpdated": "2024-07-28T22:31:17+0000",
 			"languages": [],
 			"tags": [
 				10,
