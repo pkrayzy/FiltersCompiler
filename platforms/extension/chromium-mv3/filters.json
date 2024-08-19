5a3f76b98e0ab9d0734dfef4b205470ce225647e
Filters Builder <filters@adguard.com>
2024-08-19 21:39:33 +0000

    skip ci. build from Mon Aug 19 21:39:33 UTC 2024

--- platforms/extension/chromium-mv3/filters.json
+++ platforms/extension/chromium-mv3/filters.json
@@ -358,8 +358,8 @@
 			"groupId": 1,
 			"subscriptionUrl": "https://filters.adtidy.org/extension/chromium-mv3/filters/2.txt",
 			"trustLevel": "full",
-			"version": "2.3.49.0",
-			"timeUpdated": "2024-08-19T20:31:09+0000",
+			"version": "2.3.49.1",
+			"timeUpdated": "2024-08-19T21:32:42+0000",
 			"languages": [],
 			"tags": [
 				10,
--- platforms/extension/safari/filters.js
+++ platforms/extension/safari/filters.js
@@ -358,8 +358,8 @@
 			"groupId": 1,
 			"subscriptionUrl": "https://filters.adtidy.org/extension/safari/filters/2_optimized.txt",
 			"trustLevel": "full",
-			"version": "2.3.49.0",
-			"timeUpdated": "2024-08-19T20:31:09+0000",
+			"version": "2.3.49.1",
+			"timeUpdated": "2024-08-19T21:32:42+0000",
 			"languages": [],
 			"tags": [
 				10,
