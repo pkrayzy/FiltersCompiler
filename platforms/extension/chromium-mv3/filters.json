e29e2a33345fae7aaea57f353e88cc34b2455cb3
Filters Builder <filters@adguard.com>
2024-08-29 07:37:32 +0000

    skip ci. build from Thu Aug 29 07:37:32 UTC 2024

--- platforms/extension/chromium-mv3/filters.json
+++ platforms/extension/chromium-mv3/filters.json
@@ -358,8 +358,8 @@
 			"groupId": 1,
 			"subscriptionUrl": "https://filters.adtidy.org/extension/chromium-mv3/filters/2.txt",
 			"trustLevel": "full",
-			"version": "2.3.50.32",
-			"timeUpdated": "2024-08-29T06:31:16+0000",
+			"version": "2.3.50.33",
+			"timeUpdated": "2024-08-29T07:31:24+0000",
 			"languages": [],
 			"tags": [
 				10,
@@ -712,8 +712,8 @@
 			"groupId": 4,
 			"subscriptionUrl": "https://filters.adtidy.org/extension/chromium-mv3/filters/21.txt",
 			"trustLevel": "full",
-			"version": "2.0.25.49",
-			"timeUpdated": "2024-08-29T06:33:21+0000",
+			"version": "2.0.25.50",
+			"timeUpdated": "2024-08-29T07:33:48+0000",
 			"languages": [],
 			"tags": [
 				5,
--- platforms/mac/filters.js
+++ platforms/mac/filters.js
@@ -61,8 +61,8 @@
 			"displayNumber": 1,
 			"groupId": 1,
 			"subscriptionUrl": "https://filters.adtidy.org/mac/filters/2.txt",
-			"version": "2.3.50.32",
-			"timeUpdated": "2024-08-29T06:31:16+0000",
+			"version": "2.3.50.33",
+			"timeUpdated": "2024-08-29T07:31:24+0000",
 			"languages": []
 		},
 		{
@@ -234,8 +234,8 @@
 			"platformsExcluded": [
 				"ext_chromium_mv3"
 			],
-			"version": "2.2.6.73",
-			"timeUpdated": "2024-08-29T06:33:14+0000",
+			"version": "2.2.6.74",
+			"timeUpdated": "2024-08-29T07:33:40+0000",
 			"languages": []
 		},
 		{
@@ -335,8 +335,8 @@
 			"displayNumber": 2,
 			"groupId": 4,
 			"subscriptionUrl": "https://filters.adtidy.org/mac/filters/21.txt",
-			"version": "2.0.25.49",
-			"timeUpdated": "2024-08-29T06:33:21+0000",
+			"version": "2.0.25.50",
+			"timeUpdated": "2024-08-29T07:33:48+0000",
 			"languages": []
 		},
 		{
