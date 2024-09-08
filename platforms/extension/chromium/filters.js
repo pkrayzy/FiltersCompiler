b496af9c8e730092c03011bdf9e1e749f458e135
Filters Builder <filters@adguard.com>
2024-09-08 11:37:11 +0000

    skip ci. build from Sun Sep  8 11:37:11 UTC 2024

--- platforms/extension/chromium/filters.js
+++ platforms/extension/chromium/filters.js
@@ -359,8 +359,8 @@
 			"groupId": 1,
 			"subscriptionUrl": "https://filters.adtidy.org/extension/chromium/filters/2.txt",
 			"trustLevel": "full",
-			"version": "2.3.51.81",
-			"timeUpdated": "2024-09-08T09:31:19+0000",
+			"version": "2.3.51.82",
+			"timeUpdated": "2024-09-08T11:31:17+0000",
 			"deprecated": false,
 			"languages": [],
 			"tags": [
