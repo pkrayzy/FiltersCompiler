ec859aa567e32738cbd7a3368a333d6c7c2ba29c
Filters Builder <filters@adguard.com>
2024-09-15 10:39:10 +0000

    skip ci. build from Sun Sep 15 10:39:10 UTC 2024

--- platforms/extension/firefox/filters.js
+++ platforms/extension/firefox/filters.js
@@ -359,8 +359,8 @@
 			"groupId": 1,
 			"subscriptionUrl": "https://filters.adtidy.org/extension/firefox/filters/2.txt",
 			"trustLevel": "full",
-			"version": "2.3.52.79",
-			"timeUpdated": "2024-09-15T02:31:16+0000",
+			"version": "2.3.52.80",
+			"timeUpdated": "2024-09-15T10:31:17+0000",
 			"deprecated": false,
 			"languages": [],
 			"tags": [
--- platforms/mac/filters.js
+++ platforms/mac/filters.js
@@ -61,8 +61,8 @@
 			"displayNumber": 1,
 			"groupId": 1,
 			"subscriptionUrl": "https://filters.adtidy.org/mac/filters/2.txt",
-			"version": "2.3.52.79",
-			"timeUpdated": "2024-09-15T02:31:16+0000",
+			"version": "2.3.52.80",
+			"timeUpdated": "2024-09-15T10:31:17+0000",
 			"languages": []
 		},
 		{
