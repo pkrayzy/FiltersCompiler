ac88212834ec024022239b8f6d8ce86f7f3ebd9d
pkrayzy <pkrayzy@users.noreply.github.com>
2024-10-06 16:14:40 +0000

    Update Filters on Sun Oct  6 16:14:37 UTC 2024

--- platforms/ios/filters.js
+++ platforms/ios/filters.js
@@ -639,8 +639,8 @@
 				"ext_chromium_mv3"
 			],
 			"trustLevel": "full",
-			"version": "2.0.70.76",
-			"timeUpdated": "2024-10-06T14:31:59+0000",
+			"version": "2.0.70.77",
+			"timeUpdated": "2024-10-06T15:41:55+0000",
 			"deprecated": false,
 			"languages": [],
 			"tags": [
@@ -834,8 +834,8 @@
 				"ext_chromium_mv3"
 			],
 			"trustLevel": "high",
-			"version": "2.1.33.24",
-			"timeUpdated": "2024-10-06T14:40:08+0000",
+			"version": "2.1.33.25",
+			"timeUpdated": "2024-10-06T15:50:16+0000",
 			"deprecated": false,
 			"languages": [],
 			"tags": [
@@ -1184,8 +1184,8 @@
 				"ext_chromium_mv3"
 			],
 			"trustLevel": "high",
-			"version": "2.1.32.39",
-			"timeUpdated": "2024-10-06T15:04:06+0000",
+			"version": "2.1.32.40",
+			"timeUpdated": "2024-10-06T16:14:11+0000",
 			"deprecated": false,
 			"languages": [],
 			"tags": [
