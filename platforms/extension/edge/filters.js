921d73ad8a0d03f33b373034471163e1740a9e7b
Filters Builder <filters@adguard.com>
2024-10-13 21:36:09 +0000

    skip ci. build from Sun Oct 13 21:36:08 UTC 2024

--- platforms/extension/edge/filters.js
+++ platforms/extension/edge/filters.js
@@ -359,8 +359,8 @@
 			"groupId": 1,
 			"subscriptionUrl": "https://filters.adtidy.org/extension/edge/filters/2.txt",
 			"trustLevel": "full",
-			"version": "2.3.56.23",
-			"timeUpdated": "2024-10-13T20:31:23+0000",
+			"version": "2.3.56.24",
+			"timeUpdated": "2024-10-13T21:31:18+0000",
 			"deprecated": false,
 			"languages": [],
 			"tags": [
--- platforms/mac/filters.js
+++ platforms/mac/filters.js
@@ -61,8 +61,8 @@
 			"displayNumber": 1,
 			"groupId": 1,
 			"subscriptionUrl": "https://filters.adtidy.org/mac/filters/2.txt",
-			"version": "2.3.56.23",
-			"timeUpdated": "2024-10-13T20:31:23+0000",
+			"version": "2.3.56.24",
+			"timeUpdated": "2024-10-13T21:31:18+0000",
 			"languages": []
 		},
 		{
