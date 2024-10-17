5c338651fe4321fbb6b5d23c99cd11fd1158d214
Filters Builder <filters@adguard.com>
2024-10-17 03:37:12 +0000

    skip ci. build from Thu Oct 17 03:37:12 UTC 2024

--- platforms/android/filters.js
+++ platforms/android/filters.js
@@ -359,8 +359,8 @@
 			"groupId": 1,
 			"subscriptionUrl": "https://filters.adtidy.org/android/filters/2_optimized.txt",
 			"trustLevel": "full",
-			"version": "2.3.56.71",
-			"timeUpdated": "2024-10-17T00:31:16+0000",
+			"version": "2.3.56.72",
+			"timeUpdated": "2024-10-17T03:31:18+0000",
 			"deprecated": false,
 			"languages": [],
 			"tags": [
