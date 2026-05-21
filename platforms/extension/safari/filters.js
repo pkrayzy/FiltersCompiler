3c8e056d80c9de510d689a6b49cebf26aa2d4055
pkrayzy <pkrayzy@users.noreply.github.com>
2026-05-21 06:08:56 +0000

    Update Filters on Thu May 21 06:08:55 UTC 2026

--- platforms/extension/safari/filters.js
+++ platforms/extension/safari/filters.js
@@ -649,8 +649,8 @@
 				"ios"
 			],
 			"trustLevel": "full",
-			"version": "2.1.19.58",
-			"timeUpdated": "2026-05-20T20:30:29+0000",
+			"version": "2.1.19.59",
+			"timeUpdated": "2026-05-21T06:08:20+0000",
 			"languages": [],
 			"tags": [
 				45,
