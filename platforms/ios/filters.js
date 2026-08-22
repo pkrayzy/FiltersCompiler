28454a1b13648f3482c6fd51dd1e8793b4d7e224
pkrayzy <pkrayzy@users.noreply.github.com>
2026-08-22 02:52:18 +0000

    Update Filters on Sat Aug 22 02:52:17 UTC 2026

--- platforms/ios/filters.js
+++ platforms/ios/filters.js
@@ -649,8 +649,8 @@
 				"ios"
 			],
 			"trustLevel": "full",
-			"version": "2.1.29.58",
-			"timeUpdated": "2026-08-21T03:00:21+0000",
+			"version": "2.1.29.59",
+			"timeUpdated": "2026-08-22T02:51:39+0000",
 			"languages": [],
 			"tags": [
 				45,
